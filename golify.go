3f1b022e2edf804cbb5e7e6a771bc74e11007a6d
Surattikorn Chumkaew <surattikorn@gmail.com>
2019-06-24 21:10:20 +0700

    not nil

--- golify.go
+++ golify.go
@@ -1,3 +1,40 @@
 package golify
 
+type golifyObject struct {
+    Value interface{}
+    Err   *golifyErr
+}
 
+type golifyErr struct {
+    ErrCode    int
+    ErrMessage string
+}
+
+func Init(value interface{}) golifyObject {
+    return golifyObject{
+        Value: value,
+        Err:   nil,
+    }
+}
+
+func (g golifyObject) NotNil(errCode int, errMsg string) golifyObject {
+
+    if g.Err != nil {
+        return g
+    }
+
+    if g.Value == nil {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode:errCode,
+                ErrMessage:errMsg,
+            },
+        }
+    }
+
+    return golifyObject{
+        Value: g.Value,
+        Err: nil,
+    }
+}
