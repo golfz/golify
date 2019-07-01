6dd21a60c3ccf7038bdee25f4c4681c6845cbc39
Surattikorn Chumkaew <surattikorn@gmail.com>
2019-07-01 17:06:29 +0700

    add MoreThan & LessThan function

--- golify.go
+++ golify.go
@@ -1,16 +1,18 @@
 package golify
 
+import "reflect"
+
 type golifyObject struct {
     Value interface{}
     Err   *golifyErr
 }
 
 type golifyErr struct {
-    ErrCode    int
-    ErrMessage string
+    ErrCode int
+    ErrMsg  string
 }
 
-func Init(value interface{}) golifyObject {
+func Validate(value interface{}) golifyObject {
     return golifyObject{
         Value: value,
         Err:   nil,
@@ -27,14 +29,152 @@ func (g golifyObject) NotNil(errCode int, errMsg string) golifyObject {
         return golifyObject{
             Value: g.Value,
             Err: &golifyErr{
-                ErrCode:errCode,
-                ErrMessage:errMsg,
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
             },
         }
     }
 
     return golifyObject{
         Value: g.Value,
-        Err: nil,
+        Err:   nil,
+    }
+}
+
+func (g golifyObject) IsInt(errCode int, errMsg string) golifyObject {
+
+    if g.Err != nil {
+        return g
+    }
+
+    if reflect.TypeOf(g.Value) != reflect.TypeOf(1) {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
+            },
+        }
+    }
+
+    return golifyObject{
+        Value: g.Value,
+        Err:   nil,
+    }
+}
+
+func (g golifyObject) MoreThan(min interface{}, errCode int, errMsg string) golifyObject {
+
+    if g.Err != nil {
+        return g
+    }
+
+    fMin := 1.0
+
+    if reflect.TypeOf(min) == reflect.TypeOf(1) {
+        fMin = float64(min.(int))
+
+    } else if reflect.TypeOf(min) == reflect.TypeOf(1.1) {
+        fMin = min.(float64)
+
+    } else {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
+            },
+        }
+    }
+
+    gVal := 1.0
+
+    if reflect.TypeOf(g.Value) == reflect.TypeOf(1) {
+        gVal = float64(g.Value.(int))
+
+    } else if reflect.TypeOf(g.Value) == reflect.TypeOf(1.1) {
+        gVal = g.Value.(float64)
+
+    } else {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
+            },
+        }
+    }
+
+    if gVal < fMin {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
+            },
+        }
+    }
+
+    return golifyObject{
+        Value: g.Value,
+        Err:   nil,
+    }
+}
+
+func (g golifyObject) LessThan(max interface{}, errCode int, errMsg string) golifyObject {
+
+    if g.Err != nil {
+        return g
+    }
+
+    fMax := 1.0
+
+    if reflect.TypeOf(max) == reflect.TypeOf(1) {
+        fMax = float64(max.(int))
+
+    } else if reflect.TypeOf(max) == reflect.TypeOf(1.1) {
+        fMax = max.(float64)
+
+    } else {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
+            },
+        }
+    }
+
+    gVal := 1.0
+
+    if reflect.TypeOf(g.Value) == reflect.TypeOf(1) {
+        gVal = float64(g.Value.(int))
+
+    } else if reflect.TypeOf(g.Value) == reflect.TypeOf(1.1) {
+        gVal = g.Value.(float64)
+
+    } else {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
+            },
+        }
+    }
+
+    if gVal > fMax {
+        return golifyObject{
+            Value: g.Value,
+            Err: &golifyErr{
+                ErrCode: errCode,
+                ErrMsg:  errMsg,
+            },
+        }
+    }
+
+    return golifyObject{
+        Value: g.Value,
+        Err:   nil,
     }
 }
